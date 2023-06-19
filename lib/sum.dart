5058d9e09cef3c9d2d45a25a1a51e21304b9c1d0
Iced <busarin.ice@gmail.com>
2023-06-19 14:47:16 +0700

    edit on sum.dart

--- lib/sum.dart
+++ lib/sum.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class sum extends StatefulWidget {
@@ -9,11 +12,55 @@ class sum extends StatefulWidget {
 }
 
 class _sumState extends State<sum> {
+  late DatabaseReference dbRef;
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = FirebaseDatabase.instance.ref();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HongChao'),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Montly Fee',
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold,
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget display({required Map data}) {
+    return Column(
+      children: [display_e(data: data)],
+    );
+  }
+
+  Widget display_e({required Map data}) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              data as String,
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
       ),
     );
   }
