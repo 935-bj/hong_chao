c848de5f00b63bdfcb45592cc0cfa520f9b126f9
Iced <busarin.ice@gmail.com>
2024-01-19 13:38:02 +0700

    add resource dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hong_chao/oldhome.dart';
 import 'package:hong_chao/login.dart';
-import 'package:hong_chao/wbill.dart';
-import 'package:hong_chao/ebill.dart';
-import 'package:hong_chao/seting.dart';
-import 'package:hong_chao/sum.dart';
-import 'package:hong_chao/wsum.dart';
-import 'package:hong_chao/oldLogin.dart';
-
-import 'cal.dart';
+import 'package:hong_chao/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,13 +24,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: {
-        oldhome.routeName: (context) => oldhome(auth: FirebaseAuth.instance),
-        wbill.routeName: (context) => const wbill(),
-        ebill.routeName: (context) => const ebill(),
-        seting.routeName: (context) => const seting(),
-        sum.routeName: (context) => const sum(),
-        wsum.routeName: (context) => const wsum(),
-        cal.routeName: (context) => const cal(),
         login.routeName: (context) => const login(),
       },
       initialRoute: login.routeName,
