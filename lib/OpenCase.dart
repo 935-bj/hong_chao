be47f180f1899a30fd9fd5a1be1c22c308d55542
somsom <boonyaporn.kha@student.mahidol.ac.th>
2024-01-19 14:43:09 +0700

    Add OpenCase.dart

--- lib/OpenCase.dart
+++ lib/OpenCase.dart
@@ -2,7 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class OpenCase StatefulWidget {
+class OpenCase extends StatefulWidget {
   static String routeName = '/OpenCase';
   const OpenCase({super.key});
 
