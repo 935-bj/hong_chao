95aa4a1baf007b5032b85cd1ee865b17b5d3488a
Pantaree Prasongsuk <pantareeprasongsuk@PP-ma-2.local>
2024-01-26 14:05:18 +0700

    regisstatusL

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         OpenCase.routeName: (context) => const OpenCase(),
         postScreen.routeName: ((context) => const postScreen()),
       },
-      initialRoute: home.routeName,
+      initialRoute: OpenCase.routeName,
     );
   }
 }
