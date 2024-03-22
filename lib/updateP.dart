ddc9b50ec251fd780bd62148ebee32fcaf198ac6
somsom <boonyaporn.kha@student.mahidol.ac.th>
2024-03-22 15:46:14 +0700

    Lawyer update process v.3

--- lib/updateP.dart
+++ lib/updateP.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class UpdateDialog {
+  static final dbRef = FirebaseDatabase.instance.ref().child('OpenCase');
+
   static Future<void> showUpdateDialog(BuildContext context, Map<String, dynamic> postDetail) async {
     // Initialize selection variables
     Map<String, bool> selection = {
@@ -16,17 +19,33 @@ class UpdateDialog {
 
     bool hasSelection = false;
 
+    // Retrieve existing selections from the database
+    DatabaseEvent event = await dbRef.child(postDetail['postID']).child('case process').once();
+
+    if (event.snapshot.value != null) {
+      Map<dynamic, dynamic>? values = event.snapshot.value as Map<dynamic, dynamic>?;
+      if (values != null) {
+        selection.forEach((key, value) {
+          if (values.containsKey(key)) {
+            selection[key] = values[key] as bool;
+          }
+        });
+        hasSelection = selection.containsValue(true);
+      }
+    }
+
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return AlertDialog(
-              title: Text('Update'),
+              title: Text('UPDATE CASE'),
               content: SingleChildScrollView(
                 child: ListBody(
                   children: <Widget>[
-                    Text('Would you like to update case process ${postDetail['postID']}?'),
+                    Text('Would you like to update case?'),
+                    //Text('Would you like to update post ${postDetail['postID']}?'),
                     SizedBox(height: 10),
                     // Add checkboxes for each selection
                     for (var entry in selection.entries)
@@ -57,7 +76,8 @@ class UpdateDialog {
                   child: Text('Update'),
                   onPressed: hasSelection
                       ? () {
-                          // Implement your logic here for handling the update
+                          // Update the selection in the database
+                          dbRef.child(postDetail['postID']).child('case process').set(selection);
                           Navigator.of(context).pop();
                           // Perform update action
                         }
