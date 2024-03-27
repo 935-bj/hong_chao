b070a6610dc710062f9ef0d28dddef5b2a13b93f
somsom <boonyaporn.kha@student.mahidol.ac.th>
2024-03-27 20:39:55 +0700

    notification v.3

--- lib/updateP.dart
+++ lib/updateP.dart
@@ -124,80 +124,80 @@ class UpdateDialog {
     );
   }
 
-static Future<void> showNotiDialog(
-    BuildContext context, Map<String, dynamic> postDetail) async {
-  // Initialize selection variables
-  Map<String, bool> selection = {
-    'Case Initiation': false,
-    'Evidence Gathering': false,
-    'Court Filings': false,
-    'Negotiation and Settlement': false,
-    'Pretrial Preparation': false,
-    'Post-Trial Action': false,
-    'Case Closure': false,
-    'Finish Case': false,
-  };
+  static Future<void> showNotiDialog(
+      BuildContext context, Map<String, dynamic> postDetail) async {
+    // Initialize selection variables
+    Map<String, bool> selection = {
+      'Case Initiation': false,
+      'Evidence Gathering': false,
+      'Court Filings': false,
+      'Negotiation and Settlement': false,
+      'Pretrial Preparation': false,
+      'Post-Trial Action': false,
+      'Case Closure': false,
+      'Finish Case': false,
+    };
 
-  bool hasSelection = false;
+    bool hasSelection = false;
 
-  DatabaseEvent notiEvent =
-      await notiRef.child(postDetail['postID']).child('case process').once();
+    DatabaseEvent notiEvent =
+        await notiRef.child(postDetail['postID']).child('case process').once();
 
-  if (notiEvent.snapshot.value != null) {
-    Map<dynamic, dynamic>? values =
-        notiEvent.snapshot.value as Map<dynamic, dynamic>?;
-    if (values != null) {
-      selection.forEach((key, value) {
-        if (values.containsKey(key)) {
-          selection[key] = values[key] as bool;
-        }
-      });
-      hasSelection = selection.containsValue(true);
+    if (notiEvent.snapshot.value != null) {
+      Map<dynamic, dynamic>? values =
+          notiEvent.snapshot.value as Map<dynamic, dynamic>?;
+      if (values != null) {
+        selection.forEach((key, value) {
+          if (values.containsKey(key)) {
+            selection[key] = values[key] as bool;
+          }
+        });
+        hasSelection = selection.containsValue(true);
+      }
     }
-  }
 
-  showDialog<void>(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Status'),
-        content: StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  Text('Status has been updated'),
-                  SizedBox(height: 10),
-                  // Add checkboxes for each selection
-                  for (var entry in selection.entries)
-                    ListTile(
-                      title: Text(entry.key),
-                      leading: Checkbox(
-                        value: selection[entry.key],
-                        onChanged: (value) {
-                          setState(() {
-                            selection[entry.key] = value!;
-                            hasSelection = selection.containsValue(true);
-                          });
-                        },
-                        shape: CircleBorder(),
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Status'),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Status has been updated'),
+                    SizedBox(height: 10),
+                    // Add checkboxes for each selection
+                    for (var entry in selection.entries)
+                      ListTile(
+                        title: Text(entry.key),
+                        leading: Checkbox(
+                          value: selection[entry.key],
+                          onChanged: (value) {
+                            // setState(() {
+                            //   //selection[entry.key] = value!;
+                            //   //hasSelection = selection.containsValue(true);
+                            // });
+                          },
+                          shape: CircleBorder(),
+                        ),
                       ),
-                    ),
-                ],
-              ),
-            );
-          },
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text('Close'),
-            onPressed: () {
-              Navigator.of(context).pop();
+                  ],
+                ),
+              );
             },
           ),
-        ],
-      );
-    },
-  );
-}
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
